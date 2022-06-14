be1e8df27dbc889790c08f5e3d0d6cb86b04b701
Zhongnan Su <szhongna@amazon.com>
2022-06-13 17:37:24 -0700

    demo: add ut to plugin folder
    Signed-off-by: Zhongnan Su <szhongna@amazon.com>

---
+++ plugin/src/test/java/org/opensearch/sql/plugin/rest/RestPPLQueryActionTest.java
@@ -0,0 +1,13 @@
+package org.opensearch.sql.plugin.rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RestPPLQueryActionTest {
+
+    @Test
+    public void testZhongnan() {
+        assertNull("zhongnan");
+    }
+}
